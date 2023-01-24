78784d863891dc03892ccb09305c8ebb64463675
CodingCas <casweeno2000@gmail.com>
2023-01-24 23:16:07 +0100

    Casweeney: code cleaned & _TotalSupply reverted back to _totalSupply

--- contracts/FooswapFactory.sol
+++ contracts/FooswapFactory.sol
@@ -22,10 +22,10 @@ contract FooswapFactory is IFooswapFactory {
     }
 
     function createPair(address tokenA, address tokenB) external returns (address pair) {
-        require(tokenA != tokenB, 'Fooswap: IDENTICAL_ADDRESSES');
+        require(tokenA != tokenB, "Fooswap: IDENTICAL_ADDRESSES");
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'Fooswap: ZERO_ADDRESS');
-        require(getPair[token0][token1] == address(0), 'Fooswap: PAIR_EXISTS'); // single check is sufficient
+        require(token0 != address(0), "Fooswap: ZERO_ADDRESS");
+        require(getPair[token0][token1] == address(0), "Fooswap: PAIR_EXISTS"); // single check is sufficient
         bytes memory bytecode = type(FooswapPair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
@@ -40,12 +40,12 @@ contract FooswapFactory is IFooswapFactory {
     }
 
     function setFeeTo(address _feeTo) external {
-        require(msg.sender == feeToSetter, 'Fooswap: FORBIDDEN');
+        require(msg.sender == feeToSetter, "Fooswap: FORBIDDEN");
         feeTo = _feeTo;
     }
 
     function setFeeToSetter(address _feeToSetter) external {
-        require(msg.sender == feeToSetter, 'Fooswap: FORBIDDEN');
+        require(msg.sender == feeToSetter, "Fooswap: FORBIDDEN");
         feeToSetter = _feeToSetter;
     }
 }
